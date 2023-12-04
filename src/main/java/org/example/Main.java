ec52b740fcdd56852eaeb5407c5d41371d9f6c86
amhernandezsa@cesde.net <amhernandezsa@cesde.net>
2023-12-04 15:35:09 -0500

    Se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Entradas del problema
+        String nombrbeUsuario ="Manuela Orlas";
+        short edadUsuario=18;
+        String telefonoUsuario="3128343217";
+        String documentoIdentida="1036518352";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direcionUsuario="Cra 11B # 5 a 24";
+        double costoInsripcion=150000.0;
+        float tasaDeInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+
+        //PROCESO
+        // 1. Asignar un valor especifico a una variable
+        // desde el TECLADO (Recibir datos)
+        Scanner leerDato = new Scanner(System.in);
+
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        leerDato.nextLine();
+    }
+}
